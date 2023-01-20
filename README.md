81b706bef4b0c0f42c8312913bc308685307573d
vusirikala <satyasatya123456@gmail.com>
2023-01-20 16:18:08 -0600

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Lyrical-GraphQL
-Starter project from a GraphQL course on Udemy.com
+Server stores two collections: Songs, Lyrics. Each song can have multiple lyrics. 
--- server/server.js
+++ server/server.js
@@ -8,16 +8,16 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = process.env.MONGO_URI;
 if (!MONGO_URI) {
-  throw new Error('You must provide a MongoLab URI');
+  throw new Error('You must provide a MongoDB URI');
 }
 
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URI);
 mongoose.connection
-    .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error));
+    .once('open', () => console.log('Connected to MongoDB instance.'))
+    .on('error', error => console.log('Error connecting to MongoDB:', error));
 
 app.use(bodyParser.json());
 app.use('/graphql', expressGraphQL({
