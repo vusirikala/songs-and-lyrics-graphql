309232d76783d26656e283e206b734f89c1032c5
vusirikala <satyasatya123456@gmail.com>
2023-01-20 22:47:40 -0600

    add react router

--- client/index.js
+++ client/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory, IndexRoute} from 'react-router';
 
 //Apollo client is agnostic to the rendering library (react, angular, etc.)
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import SongList from './components/SongList';
+import App from './components/App';
 
 // apollo client makes an assumption that the backend uses /grapql endpoint for graphql queries
 const client = new ApolloClient({});
@@ -12,7 +14,11 @@ const client = new ApolloClient({});
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <SongList />
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+        </Route> 
+      </Router>
     </ApolloProvider>
   )
 };
