28abd91adc49c1425a57102d7596245fee394010
vusirikala <satyasatya123456@gmail.com>
2023-01-20 18:54:33 -0600

    fixed mongodb connection issues

--- server/server.js
+++ server/server.js
@@ -4,20 +4,31 @@ const expressGraphQL = require('express-graphql');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const schema = require('./schema/schema');
-
+require('dotenv').config()
 const app = express();
 
 // Replace with your mongoLab URI
 const MONGO_URI = process.env.MONGO_URI;
 if (!MONGO_URI) {
+  mongoose.set('strictQuery', false);
   throw new Error('You must provide a MongoDB URI');
 }
 
-mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
-mongoose.connection
-    .once('open', () => console.log('Connected to MongoDB instance.'))
-    .on('error', error => console.log('Error connecting to MongoDB:', error));
+async function start() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI)  
+    console.log("Connected to MongoDB")
+  } catch (err) {
+    console.error(err)
+  }
+}
+start()
+
+// mongoose.Promise = global.Promise;
+// mongoose.connect(MONGO_URI, {useMongoClient: true});
+// mongoose.connection
+//     .once('open', () => console.log('Connected to MongoDB instance.'))
+//     .on('error', error => console.log('Error connecting to MongoDB:', error));
 
 app.use(bodyParser.json());
 app.use('/graphql', expressGraphQL({
